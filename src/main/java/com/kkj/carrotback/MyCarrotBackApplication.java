e4b86d89f5baa813b11bef61f6919a1076c245ea
kkj860710 <kkj9491@knou.ac.kr>
2025-01-17 11:46:23 +0900

    first

---
+++ src/main/java/com/kkj/carrotback/MyCarrotBackApplication.java
@@ -0,0 +1,13 @@
+package com.kkj.carrotback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCarrotBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyCarrotBackApplication.class, args);
+    }
+
+}
